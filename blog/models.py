42455675c77f57dceef816fc6777c5f3e652f98b
Nataliyi <ediga3@gmail.com>
2020-03-01 10:29:27 +0100

    Лалала

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,9 @@ from django.db import models
 from django.utils import timezone
 
 
+#просто проверяю как работает github
+
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
